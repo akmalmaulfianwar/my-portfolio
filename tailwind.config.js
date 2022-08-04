06d545acdee39f7901a19af8b01a78ce9aad40b1
akmalmaulfianwar <202010225032@mhs.ubharajaya.ac.id>
2022-08-04 15:05:10 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#8b5cf6",
+        dark: "#1e293b",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
