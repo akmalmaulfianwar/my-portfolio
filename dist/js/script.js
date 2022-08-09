528368cb4abe289376343d2e4fd301264c740c7e
akmalmaulfianwar <202010225032@mhs.ubharajaya.ac.id>
2022-08-09 15:49:17 +0700

    add dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,15 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
